ce5b710d1deb3a1c012803ff376352544c871578
Colin S <3526918+cbs228@users.noreply.github.com>
2019-04-09 22:27:51 -0500

    Use generic Command instead of Boxed
    Previously, Command was implemented using a boxed trait object.
    For performance reasons, seasoned rustaceans recommend using
    generics instead.
    
    Rewrite the command module to return generics instead of Box<...>.
    From the user's perspective, the interface is almost the same.

--- src/command.rs
+++ src/command.rs
@@ -4,22 +4,21 @@
 //! Commands are accepted when they are echoed back
 //! and rejected by `FAULT` messages.
 
-use std::boxed::Box;
 use std::convert::Into;
 use std::fmt;
 use std::string::String;
-use std::vec;
+use std::vec::Vec;
 
 use super::constants as C;
+use super::constants::{CommandID, ProtocolMode, NEWLINE_STR};
 
-/// A command sent to the TNC
-///
-/// All `Command` must produce their serialized,
-/// wireline representation via the `fmt::Display`
-/// trait.
-pub trait Command: fmt::Display {
-    /// Command identifier
-    fn command_id(&self) -> &C::CommandID;
+/// An arbitrary command sent to the TNC
+pub struct Command<I>
+where
+    I: fmt::Display,
+{
+    id: CommandID,
+    args: Option<I>,
 }
 
 /// Uncleanly abort the connection
@@ -28,10 +27,11 @@ pub trait Command: fmt::Display {
 /// FEC Send session.
 ///
 /// You should use `disconnect()` instead!
-pub fn abort() -> Box<Command> {
-    Box::new(CommandNoArg {
-        id: C::CommandID::ABORT,
-    })
+pub fn abort() -> Command<arg::NoArg> {
+    Command {
+        id: CommandID::ABORT,
+        args: None,
+    }
 }
 
 /// Set ARQ bandwidth
@@ -47,8 +47,11 @@ pub fn abort() -> Box<Command> {
 /// - `bw`: Bandwidth, in Hz. Must be supported by the TNC
 /// - `forced`: If true, use only this bandwidth and do not allow
 ///   negotiations.
-pub fn arqbw(bw: u16, forced: bool) -> Box<Command> {
-    Box::new(CommandArqBw { bw, forced })
+pub fn arqbw(bw: u16, forced: bool) -> Command<arg::ArqBw> {
+    Command {
+        id: CommandID::ARQBW,
+        args: Some(arg::ArqBw { bw, forced }),
+    }
 }
 
 /// Make a new outgoing ARQ connection attempt
@@ -61,16 +64,18 @@ pub fn arqbw(bw: u16, forced: bool) -> Box<Command> {
 /// - `target`: Call sign must be a legitimate call sign,
 ///   a tactical callsign, or "`CQ`."
 /// - `attempts`: Repeat count, 2 -- 15.
-pub fn arqcall<S>(target: S, attempts: u16) -> Box<Command>
+pub fn arqcall<S>(target: S, attempts: u16) -> Command<arg::ArqCall>
 where
     S: Into<String>,
 {
     let s = target.into();
-    Box::new(CommandArqCall {
-        id: C::CommandID::ARQCALL,
-        target: s,
-        attempts,
-    })
+    Command {
+        id: CommandID::ARQCALL,
+        args: Some(arg::ArqCall {
+            target: s,
+            attempts,
+        }),
+    }
 }
 
 /// Set ARQ connection timeout
@@ -84,11 +89,11 @@ where
 ///
 /// Parameters
 /// - `timeout`: ARQ timeout period, in seconds (30 -- 600)
-pub fn arqtimeout(timeout: u16) -> Box<Command> {
-    Box::new(CommandSingleArg {
-        id: C::CommandID::ARQTIMEOUT,
-        arg: timeout,
-    })
+pub fn arqtimeout(timeout: u16) -> Command<u16> {
+    Command {
+        id: CommandID::ARQTIMEOUT,
+        args: Some(timeout),
+    }
 }
 
 /// Enable or disable autobreak
@@ -98,11 +103,11 @@ pub fn arqtimeout(timeout: u16) -> Box<Command> {
 ///
 /// Parameters
 /// - `autobreak`: Enable automatic breaks
-pub fn autobreak(autobreak: bool) -> Box<Command> {
-    Box::new(CommandBoolArg {
-        id: C::CommandID::AUTOBREAK,
-        arg: autobreak,
-    })
+pub fn autobreak(autobreak: bool) -> Command<arg::BoolArg> {
+    Command {
+        id: CommandID::AUTOBREAK,
+        args: Some(arg::BoolArg { arg: autobreak }),
+    }
 }
 
 /// Block connections on busy channels
@@ -112,11 +117,11 @@ pub fn autobreak(autobreak: bool) -> Box<Command> {
 ///
 /// Parameters
 /// - `block`: if true, enable busy channel lockout / blocking
-pub fn busyblock(block: bool) -> Box<Command> {
-    Box::new(CommandBoolArg {
-        id: C::CommandID::BUSYBLOCK,
-        arg: block,
-    })
+pub fn busyblock(block: bool) -> Command<arg::BoolArg> {
+    Command {
+        id: CommandID::BUSYBLOCK,
+        args: Some(arg::BoolArg { arg: block }),
+    }
 }
 
 /// Busy detector threshold value
@@ -130,11 +135,11 @@ pub fn busyblock(block: bool) -> Box<Command> {
 /// Parameters
 /// - `level`: Busy detector threshold (0 -- 10). A value of 0 will disable
 ///   the busy detector (not recommended).
-pub fn busydet(level: u16) -> Box<Command> {
-    Box::new(CommandSingleArg {
-        id: C::CommandID::BUSYDET,
-        arg: level,
-    })
+pub fn busydet(level: u16) -> Command<u16> {
+    Command {
+        id: CommandID::BUSYDET,
+        args: Some(level),
+    }
 }
 
 /// Send CW after ID frames
@@ -146,11 +151,23 @@ pub fn busydet(level: u16) -> Box<Command> {
 ///
 /// Parameters
 /// - `cw`: Send CW ID with ARDOP digital ID frames
-pub fn cwid(cw: bool) -> Box<Command> {
-    Box::new(CommandBoolArg {
-        id: C::CommandID::CWID,
-        arg: cw,
-    })
+pub fn cwid(cw: bool) -> Command<arg::BoolArg> {
+    Command {
+        id: CommandID::CWID,
+        args: Some(arg::BoolArg { arg: cw }),
+    }
+}
+
+/// Start disconnect
+///
+/// Starts an orderly tear-down of an ARQ connection.
+/// Disconnection will be confirmed with the remote peer,
+/// if possible.
+pub fn disconnect() -> Command<arg::NoArg> {
+    Command {
+        id: CommandID::DISCONNECT,
+        args: None,
+    }
 }
 
 /// Set your station's grid square
@@ -163,25 +180,26 @@ pub fn cwid(cw: bool) -> Box<Command> {
 ///
 /// Parameters
 /// - `grid`: Your grid square (4, 6, or 8-characters).
-pub fn gridsquare<S>(grid: S) -> Box<Command>
+pub fn gridsquare<S>(grid: S) -> Command<String>
 where
     S: Into<String>,
 {
     let s = grid.into();
-    Box::new(CommandSingleArg {
-        id: C::CommandID::GRIDSQUARE,
-        arg: s,
-    })
+    Command {
+        id: CommandID::GRIDSQUARE,
+        args: Some(s),
+    }
 }
 
 /// Clears any pending queued values in the TNC interface
 ///
 /// All new TCP connections to the TNC should start with this command.
 /// This command resets the TNC to initial conditions.
-pub fn initialize() -> Box<Command> {
-    Box::new(CommandNoArg {
-        id: C::CommandID::INITIALIZE,
-    })
+pub fn initialize() -> Command<arg::NoArg> {
+    Command {
+        id: CommandID::INITIALIZE,
+        args: None,
+    }
 }
 
 /// Leader tone duration
@@ -192,11 +210,11 @@ pub fn initialize() -> Box<Command> {
 ///
 /// Parameters
 /// - `duration`: Leader tone duration, milliseconds
-pub fn leader(duration: u16) -> Box<Command> {
-    Box::new(CommandSingleArg {
-        id: C::CommandID::LEADER,
-        arg: duration,
-    })
+pub fn leader(duration: u16) -> Command<u16> {
+    Command {
+        id: CommandID::LEADER,
+        args: Some(duration),
+    }
 }
 
 /// Listen for incoming connections
@@ -210,11 +228,11 @@ pub fn leader(duration: u16) -> Box<Command> {
 ///
 /// Parameters
 /// - `listen`: Enable listening
-pub fn listen(listen: bool) -> Box<Command> {
-    Box::new(CommandBoolArg {
-        id: C::CommandID::LISTEN,
-        arg: listen,
-    })
+pub fn listen(listen: bool) -> Command<arg::BoolArg> {
+    Command {
+        id: CommandID::LISTEN,
+        args: Some(arg::BoolArg { arg: listen }),
+    }
 }
 
 /// Set your station's auxiliary callsigns
@@ -229,8 +247,11 @@ pub fn listen(listen: bool) -> Box<Command> {
 /// Parameters:
 /// - `aux`: Vector of auxiliary callsigns. If empty, all aux callsigns
 ///   will be removed.
-pub fn myaux(aux: vec::Vec<String>) -> Box<Command> {
-    Box::new(CommandMyAux { aux })
+pub fn myaux(aux: Vec<String>) -> Command<arg::MyAux> {
+    Command {
+        id: CommandID::MYAUX,
+        args: Some(arg::MyAux { aux }),
+    }
 }
 
 /// Set your station's callsign
@@ -242,15 +263,15 @@ pub fn myaux(aux: vec::Vec<String>) -> Box<Command> {
 ///
 /// Parameters
 /// - `callsign`: Assigned, proper callsign for this station
-pub fn mycall<S>(callsign: S) -> Box<Command>
+pub fn mycall<S>(callsign: S) -> Command<String>
 where
     S: Into<String>,
 {
     let s = callsign.into();
-    Box::new(CommandSingleArg {
-        id: C::CommandID::MYCALL,
-        arg: s,
-    })
+    Command {
+        id: CommandID::MYCALL,
+        args: Some(s),
+    }
 }
 
 /// Send a ping request
@@ -263,16 +284,18 @@ where
 /// Parameters
 /// - `target`: Target callsign, which may be a tactical call.
 /// - `attempts`: Repeat count, 2 -- 15.
-pub fn ping<S>(target: S, attempts: u16) -> Box<Command>
+pub fn ping<S>(target: S, attempts: u16) -> Command<arg::ArqCall>
 where
     S: Into<String>,
 {
     let s = target.into();
-    Box::new(CommandArqCall {
-        id: C::CommandID::PING,
-        target: s,
-        attempts,
-    })
+    Command {
+        id: CommandID::PING,
+        args: Some(arg::ArqCall {
+            target: s,
+            attempts,
+        }),
+    }
 }
 
 /// Set protocol mode
@@ -281,202 +304,133 @@ where
 ///
 /// Parameters
 /// - `mode`: `FEC` or `ARQ`
-pub fn protocolmode(mode: C::ProtocolMode) -> Box<Command> {
-    Box::new(CommandProtocolMode { mode })
+pub fn protocolmode(mode: ProtocolMode) -> Command<ProtocolMode> {
+    Command {
+        id: CommandID::PROTOCOLMODE,
+        args: Some(mode),
+    }
 }
 
 /// Send ID frame
 ///
 /// Sends an ID frame immediately, followed by a CW ID (if `CWID` is set)
-pub fn sendid() -> Box<Command> {
-    Box::new(CommandNoArg {
-        id: C::CommandID::SENDID,
-    })
+pub fn sendid() -> Command<arg::NoArg> {
+    Command {
+        id: CommandID::SENDID,
+        args: None,
+    }
 }
 
 /// Start a two-tone test
 ///
 /// Send 5 second two-tone burst, at the normal leader amplitude. May
 /// be used in adjusting drive level to the radio.
-pub fn twotonetest() -> Box<Command> {
-    Box::new(CommandNoArg {
-        id: C::CommandID::TWOTONETEST,
-    })
+pub fn twotonetest() -> Command<arg::NoArg> {
+    Command {
+        id: CommandID::TWOTONETEST,
+        args: None,
+    }
 }
 
 /// Query version
 ///
 /// Query the software version of the TNC.
-pub fn version() -> Box<Command> {
-    Box::new(CommandNoArg {
-        id: C::CommandID::VERSION,
-    })
+pub fn version() -> Command<arg::NoArg> {
+    Command {
+        id: CommandID::VERSION,
+        args: None,
+    }
 }
 
-/// A command with no arguments
-struct CommandNoArg {
-    /// Command ID
-    id: C::CommandID,
-}
-
-/// A command with a boolean argument
-struct CommandBoolArg {
-    id: C::CommandID,
-    arg: bool,
-}
-
-/// A command with a single argument of any formatable type
-struct CommandSingleArg<T>
+impl<I> Command<I>
 where
-    T: fmt::Display,
+    I: fmt::Display,
 {
-    id: C::CommandID,
-    arg: T,
-}
-
-/// Set ARQ bandwidth negotiation
-struct CommandArqBw {
-    bw: u16,
-    forced: bool,
-}
-
-/// Make an outgoing ARQ connection attempt
-struct CommandArqCall {
-    id: C::CommandID,
-    target: String,
-    attempts: u16,
-}
-
-/// Set auxiliary callsign(s)
-struct CommandMyAux {
-    aux: vec::Vec<String>,
-}
-
-/// Set protocol mode
-struct CommandProtocolMode {
-    /// New protocol mode for the TNC
-    mode: C::ProtocolMode,
-}
-
-impl Command for CommandNoArg {
-    fn command_id(&self) -> &C::CommandID {
+    /// Command identifier
+    pub fn command_id(&self) -> &CommandID {
         &self.id
     }
 }
 
-impl fmt::Display for CommandNoArg {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}{}", self.command_id(), C::NEWLINE_STR)
-    }
-}
-
-impl Command for CommandBoolArg {
-    fn command_id(&self) -> &C::CommandID {
-        &self.id
-    }
-}
-
-impl fmt::Display for CommandBoolArg {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            f,
-            "{} {}{}",
-            self.command_id(),
-            C::truth_str(self.arg),
-            C::NEWLINE_STR
-        )
-    }
-}
-
-impl<T> Command for CommandSingleArg<T>
+// generic formatter
+impl<I> fmt::Display for Command<I>
 where
-    T: fmt::Display,
-{
-    fn command_id(&self) -> &C::CommandID {
-        &self.id
-    }
-}
-
-impl<T> fmt::Display for CommandSingleArg<T>
-where
-    T: fmt::Display,
+    I: fmt::Display,
 {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{} {}{}", self.command_id(), self.arg, C::NEWLINE_STR)
-    }
-}
-
-impl Command for CommandArqBw {
-    fn command_id(&self) -> &C::CommandID {
-        &C::CommandID::ARQBW
-    }
-}
-
-impl fmt::Display for CommandArqBw {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        const FORCED: &'static [&'static str] = &["MAX", "FORCED"];
-
-        write!(
-            f,
-            "{} {}{}{}",
-            self.command_id(),
-            self.bw,
-            FORCED[self.forced as usize],
-            C::NEWLINE_STR
-        )
-    }
-}
-
-impl Command for CommandArqCall {
-    fn command_id(&self) -> &C::CommandID {
-        &self.id
-    }
-}
-
-impl fmt::Display for CommandArqCall {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            f,
-            "{} {} {}{}",
-            self.command_id(),
-            self.target,
-            self.attempts,
-            C::NEWLINE_STR
-        )
-    }
-}
-
-impl Command for CommandMyAux {
-    fn command_id(&self) -> &C::CommandID {
-        &C::CommandID::MYAUX
-    }
-}
-
-impl fmt::Display for CommandMyAux {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        if self.aux.is_empty() {
-            write!(f, "{} X{}", self.command_id(), C::NEWLINE_STR)
-        } else {
-            write!(
-                f,
-                "{} {}{}",
-                self.command_id(),
-                self.aux.join(","),
-                C::NEWLINE_STR
-            )
+        match &self.args {
+            None => write!(f, "{}{}", self.command_id(), NEWLINE_STR),
+            Some(x) => write!(f, "{} {}{}", self.command_id(), x, NEWLINE_STR),
         }
     }
 }
 
-impl Command for CommandProtocolMode {
-    fn command_id(&self) -> &C::CommandID {
-        &C::CommandID::PROTOCOLMODE
-    }
-}
+mod arg {
+    use super::C;
 
-impl fmt::Display for CommandProtocolMode {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{} {}{}", self.command_id(), self.mode, C::NEWLINE_STR)
+    use std::fmt;
+    use std::string::String;
+    use std::vec;
+
+    // No arguments
+    pub struct NoArg {}
+
+    impl fmt::Display for NoArg {
+        fn fmt(&self, _f: &mut fmt::Formatter) -> fmt::Result {
+            Ok(())
+        }
+    }
+
+    // A single boolean argument
+    pub struct BoolArg {
+        pub arg: bool,
+    }
+
+    impl fmt::Display for BoolArg {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "{}", C::truth_str(self.arg),)
+        }
+    }
+
+    // Set ARQ bandwidth negotiation
+    pub struct ArqBw {
+        pub bw: u16,
+        pub forced: bool,
+    }
+
+    impl fmt::Display for ArqBw {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            const FORCED: &'static [&'static str] = &["MAX", "FORCED"];
+
+            write!(f, "{}{}", self.bw, FORCED[self.forced as usize])
+        }
+    }
+
+    // Make an outgoing ARQ connection attempt
+    pub struct ArqCall {
+        pub target: String,
+        pub attempts: u16,
+    }
+
+    impl fmt::Display for ArqCall {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "{} {}", self.target, self.attempts)
+        }
+    }
+
+    // Set auxiliary callsign(s)
+    pub struct MyAux {
+        pub aux: vec::Vec<String>,
+    }
+
+    impl fmt::Display for MyAux {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            if self.aux.is_empty() {
+                write!(f, "X")
+            } else {
+                write!(f, "{}", self.aux.join(","))
+            }
+        }
     }
 }
 
@@ -525,7 +479,7 @@ mod test {
 
     #[test]
     fn test_protocolmode() {
-        let cmd = protocolmode(C::ProtocolMode::FEC);
+        let cmd = protocolmode(ProtocolMode::FEC);
         let cmdstring = format!("{}", cmd);
         assert_eq!("PROTOCOLMODE FEC\r", cmdstring);
     }
