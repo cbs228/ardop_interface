03e41c19f717610b53521b33c5efd69ba3de5540
Colin S <3526918+cbs228@users.noreply.github.com>
2019-05-25 17:04:42 -0500

    Add combined Framer
    Oddly enough, the ReadHalf and WriteHalf output from a socket
    split() are not Unpin. This prevents us from using this type with
    our FramedRead and FramedWrite.
    
    Make a combined Framer which can just hold the io object.
    TcpStream itself is Unpin.

--- src/framing/control.rs
+++ src/framing/control.rs
@@ -49,10 +49,11 @@ mod test {
     use std::io::Cursor;
     use std::str;
 
+    use futures::executor;
     use futures::executor::ThreadPool;
     use futures::prelude::*;
 
-    use super::super::framer::{FramedRead, FramedWrite};
+    use super::super::framer::{Framed, FramedRead, FramedWrite};
     use crate::protocol::response::{Event, Response};
 
     #[test]
@@ -81,6 +82,37 @@ mod test {
 
         let mut exec = ThreadPool::new().expect("Failed to create threadpool");
         exec.run(async {
+            await!(framer.send("MYCALL W1AW\r".to_owned())).unwrap();
+            await!(framer.send("LISTEN TRUE\r".to_owned())).unwrap();
+        });
+        let (curs, _) = framer.release();
+        assert_eq!(
+            "MYCALL W1AW\rLISTEN TRUE\r",
+            str::from_utf8(curs.into_inner().as_ref()).unwrap()
+        );
+    }
+
+    #[test]
+    fn test_encode_decode() {
+        let words = b"PENDING\rCANCELPENDING\r".to_vec();
+        let curs = Cursor::new(words);
+        let mut framer = Framed::new(curs, TncControlFraming::new());
+
+        executor::block_on(async {
+            let e1 = await!(framer.next());
+            assert_eq!(Response::Event(Event::PENDING), e1.unwrap());
+
+            let e2 = await!(framer.next());
+            assert_eq!(Response::Event(Event::CANCELPENDING), e2.unwrap());
+
+            let e3 = await!(framer.next());
+            assert!(e3.is_none());
+        });
+
+        let curs = Cursor::new(vec![0u8; 24]);
+        let mut framer = Framed::new(curs, TncControlFraming::new());
+
+        executor::block_on(async {
             await!(framer.send("MYCALL W1AW\r".to_owned())).unwrap();
             await!(framer.send("LISTEN TRUE\r".to_owned())).unwrap();
         });
--- src/framing/framer.rs
+++ src/framing/framer.rs
@@ -125,10 +125,6 @@ where
     fn poll_flush(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Result<(), Self::SinkError>> {
         // transmit bytes to socket
         let this = self.get_mut();
-        if this.buf.is_empty() {
-            // nothing to send
-
-        }
 
         while !this.buf.is_empty() {
             let num_written = ready!(Pin::new(&mut this.io).poll_write(cx, this.buf.as_ref()))?;
@@ -253,4 +249,151 @@ where
             }
         }
     }
+}
+
+/// Converts messages to byte stream, and back again
+pub struct Framed<I, C>
+where
+    I: AsyncRead + AsyncWrite + Unpin,
+    C: Encoder + Decoder,
+{
+    codec: C,
+    io: I,
+    inbuf: BytesMut,
+    outbuf: BytesMut,
+}
+
+impl<I, C> Framed<I, C>
+where
+    I: AsyncRead + AsyncWrite + Unpin,
+    C: Encoder + Decoder,
+{
+    /// New framer for I/O
+    ///
+    /// Creates a new framer which converts between messages
+    /// and bytes on the given `io`.
+    ///
+    /// # Parameter
+    /// - `io`: A socket or other `AsyncRead` and `AsyncWrite` type
+    /// - `codec`: The encoder used to transform messages
+    ///   into bytes
+    pub fn new(io: I, codec: C) -> Self {
+        Framed {
+            codec,
+            io,
+            inbuf: BytesMut::with_capacity(READ_SIZE),
+            outbuf: BytesMut::with_capacity(READ_SIZE),
+        }
+    }
+
+    /// Release the I/O and the codec
+    ///
+    /// Destroys this `FramedRead` and returns its components
+    pub fn release(self) -> (I, C) {
+        (self.io, self.codec)
+    }
+}
+
+impl<I, C> Sink<C::EncodeItem> for Framed<I, C>
+where
+    I: AsyncRead + AsyncWrite + Unpin,
+    C: Encoder + Decoder,
+{
+    type SinkError = io::Error;
+
+    fn poll_ready(
+        self: Pin<&mut Self>,
+        _cx: &mut Context<'_>,
+    ) -> Poll<Result<(), Self::SinkError>> {
+        // we are always ready to receive
+        Poll::Ready(Ok(()))
+    }
+
+    fn start_send(self: Pin<&mut Self>, item: C::EncodeItem) -> Result<(), Self::SinkError> {
+        // encode the item
+        let this = self.get_mut();
+        this.codec.encode(item, &mut this.outbuf)
+    }
+
+    fn poll_flush(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Result<(), Self::SinkError>> {
+        // transmit bytes to socket
+        let this = self.get_mut();
+
+        while !this.outbuf.is_empty() {
+            let num_written = ready!(Pin::new(&mut this.io).poll_write(cx, this.outbuf.as_ref()))?;
+            let _ = this.outbuf.split_to(num_written);
+
+            if num_written <= 0 {
+                // a write of zero bytes is sometimes an EOF
+                return Poll::Ready(Err(io::Error::new(
+                    io::ErrorKind::UnexpectedEof,
+                    "End of file",
+                )));
+            }
+        }
+
+        // nothing further to send
+        return Poll::Ready(Ok(()));
+    }
+
+    fn poll_close(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Result<(), Self::SinkError>> {
+        ready!(Pin::new(&mut *self).poll_flush(cx))?;
+        Pin::new(&mut (*self).io).poll_close(cx)
+    }
+}
+
+impl<I, C> Stream for Framed<I, C>
+where
+    I: AsyncRead + AsyncWrite + Unpin,
+    C: Encoder + Decoder,
+{
+    type Item = C::DecodeItem;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        let this = self.get_mut();
+
+        // try to frame from the buffer
+        let mut buf = &mut this.inbuf;
+        if !buf.is_empty() {
+            match this.codec.decode(&mut buf) {
+                Ok(Some(itm)) => return Poll::Ready(Some(itm)),
+                Err(_e) => return Poll::Ready(None),
+                Ok(None) => { /* continue */ }
+            }
+        }
+
+        loop {
+            // enlarge the buffer by READ_SIZE, and read that much
+            let old_len = buf.len();
+            buf.resize(old_len + READ_SIZE, 0u8);
+            match ready!(Pin::new(&mut this.io).poll_read(cx, &mut buf.as_mut()[old_len..])) {
+                Ok(nread) => {
+                    // shrink the buffer back down
+                    buf.resize(old_len + nread, 0u8);
+                    if nread <= 0 {
+                        // EOF
+                        return Poll::Ready(None);
+                    }
+                }
+                Err(_e) => {
+                    buf.resize(old_len, 0u8);
+                    return Poll::Ready(None);
+                }
+            }
+
+            // try to frame
+            match this.codec.decode(&mut buf) {
+                Ok(Some(itm)) => return Poll::Ready(Some(itm)),
+                Err(_e) => return Poll::Ready(None),
+                Ok(None) => continue,
+            }
+        }
+    }
+}
+
+impl<I, C> Unpin for Framed<I, C>
+where
+    I: AsyncRead + AsyncWrite + Unpin,
+    C: Encoder + Decoder,
+{
 }
