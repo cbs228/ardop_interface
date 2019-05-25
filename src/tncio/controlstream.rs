d3bd70a9918ef1b028bf72737a7b377275619771
Colin S <3526918+cbs228@users.noreply.github.com>
2019-05-25 17:28:40 -0500

    Make the controlstream also be a sink
    In order to avoid split()-ting the I/O type, the control stream
    also needs to include a Sink which can send.
    
    * Switch from FramedRead -> Framed
    * Add a third return value to controlstream(), which is a
      passthru Sink to the Framed sink.

--- src/tncio/controlstream.rs
+++ src/tncio/controlstream.rs
@@ -3,9 +3,11 @@
 //! This module exposes a `controlstream(AsyncRead)` method
 //! which
 //! 1. frames the TNC control port socket; and
-//! 2. splits the output into events and command responses
+//! 2. splits the output into events and command responses,
+//!    and accepts Commands for transmission to the socket
 
 use std::collections::vec_deque::VecDeque;
+use std::io;
 use std::marker::Unpin;
 use std::pin::Pin;
 use std::sync::{Arc, Mutex};
@@ -15,7 +17,7 @@ use futures::task::Context;
 use futures::task::Poll;
 
 use crate::framing::control::TncControlFraming;
-use crate::framing::framer::FramedRead;
+use crate::framing::framer::Framed;
 use crate::protocol::response::{CommandResult, Event, Response};
 
 /// Bind input streams to a TNC control socket
@@ -25,18 +27,25 @@ use crate::protocol::response::{CommandResult, Event, Response};
 /// `Event`s and one for `CommandResult`s.
 ///
 /// # Parameters
-/// - `control_in`: TNC control port (read half)
+/// - `control`: TNC control port (io type)
 ///
 /// # Returns
 /// 0. A `Stream` of asynchronous `Event`s from the TNC
-/// 1. A `Stream` of good/bad responses to `Commands` sent
-///    by the caller.
-pub fn controlstream<R>(control_in: R) -> (ControlStreamEvents<R>, ControlStreamResults<R>)
+/// 1. A `Stream` of good/bad responses to `Command`s sent
+/// 2. A `Sink` which will accept stringified `Command`s for
+///    transmission
+pub fn controlstream<I>(
+    control: I,
+) -> (
+    ControlStreamEvents<I>,
+    ControlStreamResults<I>,
+    ControlSink<I>,
+)
 where
-    R: AsyncRead + Unpin,
+    I: AsyncRead + AsyncWrite + Unpin,
 {
     let state = ControlState {
-        control_in: FramedRead::new(control_in, TncControlFraming::new()),
+        io: Framed::new(control, TncControlFraming::new()),
         event_queue: VecDeque::with_capacity(32),
         result_queue: VecDeque::with_capacity(32),
         fused: false,
@@ -48,45 +57,59 @@ where
         state: stateref.clone(),
     };
 
-    let results = ControlStreamResults { state: stateref };
+    let results = ControlStreamResults {
+        state: stateref.clone(),
+    };
 
-    (ctrl, results)
+    let sink = ControlSink { state: stateref };
+
+    (ctrl, results, sink)
 }
 
 // Holds state for the control streams
-struct ControlState<R>
+struct ControlState<I>
 where
-    R: AsyncRead + Unpin,
+    I: AsyncRead + AsyncWrite + Unpin,
 {
-    control_in: FramedRead<R, TncControlFraming>,
+    io: Framed<I, TncControlFraming>,
     event_queue: VecDeque<Event>,
     result_queue: VecDeque<CommandResult>,
     fused: bool,
 }
 
 /// Stream of TNC Connection Events
-pub struct ControlStreamEvents<R>
+pub struct ControlStreamEvents<I>
 where
-    R: AsyncRead + Unpin,
+    I: AsyncRead + AsyncWrite + Unpin,
 {
-    state: Arc<Mutex<ControlState<R>>>,
+    state: Arc<Mutex<ControlState<I>>>,
 }
 
 /// Stream of TNC Command Results
-pub struct ControlStreamResults<R>
+pub struct ControlStreamResults<I>
 where
-    R: AsyncRead + Unpin,
+    I: AsyncRead + AsyncWrite + Unpin,
 {
-    state: Arc<Mutex<ControlState<R>>>,
+    state: Arc<Mutex<ControlState<I>>>,
 }
 
-impl<R> Unpin for ControlStreamEvents<R> where R: AsyncRead + Unpin {}
-
-impl<R> Unpin for ControlStreamResults<R> where R: AsyncRead + Unpin {}
-
-impl<R> Stream for ControlStreamEvents<R>
+/// Sink for outgoing TNC commands
+pub struct ControlSink<I>
 where
-    R: AsyncRead + Unpin,
+    I: AsyncRead + AsyncWrite + Unpin,
+{
+    state: Arc<Mutex<ControlState<I>>>,
+}
+
+impl<I> Unpin for ControlStreamEvents<I> where I: AsyncRead + AsyncWrite + Unpin {}
+
+impl<I> Unpin for ControlStreamResults<I> where I: AsyncRead + AsyncWrite + Unpin {}
+
+impl<I> Unpin for ControlSink<I> where I: AsyncRead + AsyncWrite + Unpin {}
+
+impl<I> Stream for ControlStreamEvents<I>
+where
+    I: AsyncRead + AsyncWrite + Unpin,
 {
     type Item = Event;
 
@@ -111,9 +134,9 @@ where
     }
 }
 
-impl<R> Stream for ControlStreamResults<R>
+impl<I> Stream for ControlStreamResults<I>
 where
-    R: AsyncRead + Unpin,
+    I: AsyncRead + AsyncWrite + Unpin,
 {
     type Item = CommandResult;
 
@@ -138,9 +161,9 @@ where
     }
 }
 
-impl<R> Stream for ControlState<R>
+impl<I> Stream for ControlState<I>
 where
-    R: AsyncRead + Unpin,
+    I: AsyncRead + AsyncWrite + Unpin,
 {
     type Item = ();
 
@@ -149,7 +172,7 @@ where
             return Poll::Ready(None);
         }
 
-        match ready!(Pin::new(&mut (*self).control_in).poll_next(cx)) {
+        match ready!(Pin::new(&mut (*self).io).poll_next(cx)) {
             None => {
                 (*self).fused = true;
                 Poll::Ready(None)
@@ -166,23 +189,53 @@ where
     }
 }
 
+impl<I> Sink<String> for ControlSink<I>
+where
+    I: AsyncRead + AsyncWrite + Unpin,
+{
+    type SinkError = io::Error;
+
+    fn poll_ready(
+        self: Pin<&mut Self>,
+        _cx: &mut Context<'_>,
+    ) -> Poll<Result<(), Self::SinkError>> {
+        // we are always ready to receive
+        Poll::Ready(Ok(()))
+    }
+
+    fn start_send(self: Pin<&mut Self>, item: String) -> Result<(), Self::SinkError> {
+        // encode the item
+        let mut state = (*self).state.lock().unwrap();
+        Pin::new(&mut state.io).start_send(item)
+    }
+
+    fn poll_flush(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Result<(), Self::SinkError>> {
+        let mut state = (*self).state.lock().unwrap();
+        Pin::new(&mut state.io).poll_flush(cx)
+    }
+
+    fn poll_close(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Result<(), Self::SinkError>> {
+        let mut state = (*self).state.lock().unwrap();
+        Pin::new(&mut state.io).poll_close(cx)
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
 
     use std::io::Cursor;
 
-    use futures::executor::ThreadPool;
+    use futures::executor;
 
     use crate::protocol::constants::CommandID;
 
     #[test]
-    fn test_stream_events() {
+    fn test_streams() {
         let curs = Cursor::new(b"MYCALL\rBUSY TRUE\rARQBW\r".to_vec());
-        let (mut evt, mut res) = controlstream(curs);
+        let (mut evt, mut res, _out) = controlstream(curs);
 
-        let mut exec = ThreadPool::new().expect("Failed to create threadpool");
-        exec.run(async {
+        executor::block_on(async {
             let e1 = await!(evt.next()).unwrap();
             assert_eq!(Event::BUSY(true), e1);
 
@@ -195,5 +248,14 @@ mod test {
             assert!(await!(res.next()).is_none());
             assert!(await!(evt.next()).is_none());
         });
+    }
+
+    #[test]
+    fn test_sink() {
+        // since there's no way to get the io back, this test
+        // mostly just ensures that a send can complete
+        let curs = Cursor::new(vec![0u8; 24]);
+        let (_evt, _res, mut sink) = controlstream(curs);
+        let _ = executor::block_on(sink.send("ABORT\r".to_owned()));
     }
 }
