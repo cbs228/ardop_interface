a3ee824e65a2855657dfec6225571debc1275d74
Colin S <3526918+cbs228@users.noreply.github.com>
2019-06-01 17:04:12 -0500

    TNC: Add disconnect method
    Returns a future which will disconnect any in-progress ARQ
    connection. Errors pass silently.

--- src/tncio/asynctnc.rs
+++ src/tncio/asynctnc.rs
@@ -52,8 +52,11 @@ const TCP_KEEPALIVE: Duration = Duration::from_secs(15);
 // Default timeout for local TNC commands
 const DEFAULT_TIMEOUT_COMMAND: Duration = Duration::from_millis(2000);
 
-// Default timeout for TNC event resolution, such as connect or disconnect
+// Default timeout for TNC event resolution, such as connect
 const DEFAULT_TIMEOUT_EVENT: Duration = Duration::from_secs(90);
+
+// Turn `DISCONNECT` into `ABORT` after this period
+const DEFAULT_TIMEOUT_DISCONNECT: Duration = Duration::from_secs(60);
 
 /// Asynchronous ARDOP TNC
 ///
@@ -494,6 +497,38 @@ where
         // timed out
         self.command(command::listen(false)).await?;
         Ok(Err(ConnectionFailedReason::NoAnswer))
+    }
+
+    /// Disconnect any in-progress ARQ connection
+    ///
+    /// When this future has returned, the ARDOP TNC has
+    /// disconnected from any in-progress ARQ session.
+    /// This method is safe to call even when no
+    /// connection is in progress. Any errors which
+    /// result from the disconnection process are ignored.
+    pub async fn disconnect(&mut self) {
+        match self.command(command::disconnect()).await {
+            Ok(()) => { /* no-op */ }
+            Err(_e) => return,
+        };
+
+        for _i in 0..2 {
+            match self
+                .next_state_change_timeout(DEFAULT_TIMEOUT_DISCONNECT.clone())
+                .await
+            {
+                Err(_timeout) => {
+                    // disconnect timeout; try to abort
+                    warn!(target:"tnc", "Disconnect timed out. Trying to abort.");
+                    let _ = self.command(command::abort()).await;
+                    continue;
+                }
+                Ok(ConnectionStateChange::Closed) => {
+                    break;
+                }
+                _ => { /* no-op */ }
+            }
+        }
     }
 
     /// Query TNC version
